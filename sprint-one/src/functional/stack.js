48c808bcf4fdf06ec01911d21e84329689aba15f
rdebeer1 <Robert.DeBeer@gmail.com>
2018-01-17 11:39:47 -0600

    Passes all stack test

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,20 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size = 0;
+  var size = 0; 
 
   // Implement the methods below
   someInstance.push = function(value) {
-    storage.size = value;
+    storage[size] = value;    
     size++;
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      var lastIndex = parseInt(size) - 1;
+      size--;
+    }
+    return storage[lastIndex];
   };
 
   someInstance.size = function() {
     return size;
   };
-
   return someInstance;
 };
