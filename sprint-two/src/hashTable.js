63a69b0f2ae909b07531cddf0249eb513e106f98
rdebeer1 <Robert.DeBeer@gmail.com>
2018-01-20 16:32:02 -0600

    tries to add resize method

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,7 +2,9 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this._count = 0;
   this._storage = LimitedArray(this._limit);
+  
 };
 
 HashTable.prototype.insert = function(k, v) {
@@ -11,41 +13,86 @@ HashTable.prototype.insert = function(k, v) {
     return undefined;
   }
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var tuple = [k, v];
   var bucket = this._storage.get(index);
-  if (bucket) {
-    for (i = 0; i < bucket.length; i++) {
-      if (bucket[i][0] === k) {
-        bucket[i][1] = v; 
-      } else {
-        bucket.push(tuple);  
-      }
+  if (!bucket) {
+    bucket = [];
+    this._storage.set(index, bucket);
+  }
+  var found = false;
+  for (i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      tuple[1] = v; 
+      found = true;
+      break;  
+    } 
+  }
+  if (!found) {
+    bucket.push([k, v]);
+    this._count++;
+    if (this._count > this._limit * 0.75) {
+      this._resize(this._limit * 2);
     }
-  } else {
-    this._storage.set(index, [tuple]);
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
+  if (!bucket) {
+    return null;
+  }
   for (i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
-      return bucket[i][1];
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
     }
   }
+  return null;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
+  if (!bucket) {
+    return null;
+  }
   for (i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
-      bucket[i].splice(i, 1);
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      bucket.splice(i, 1);
+      this._count--;
+      if (this._count < this._limit * 0.25) {
+        this._resize(this._limit / 2);
+      }
+      return tuple[1];
     }
   }
+  return null;
 };
 
+HashTable.prototype._resize = function(newLimit) {
+  var oldStorage = this._storage;
+  this._limit = newLimit;
+  this._storage = LimitedArray(newLimit);
+  this._count = 0;
+  //   for (var i = 0; i < this._limit; i++) {
+  //     if (this._storage.get(i)) {
+  //       return;
+  //     }
+  //   }
+  // };
+
+  oldStorage.each(function(bucket) {
+    if (!bucket) { 
+      return;
+    } 
+    for (i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      this.insert(tuple[0], tuple[1]);
+    }
+  }.bind(this));
+};
 /*
  
 * Complexity: What is the time complexity of the above functions?
